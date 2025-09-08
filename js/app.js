0ed13a75bd6c33e84db081920cf33319aef70246
Shadman Jahin <jahin.web@gmail.com>
2025-09-08 17:01:53 +0600

    project basic completed

--- js/app.js
+++ js/app.js
@@ -11,11 +11,41 @@ document.addEventListener('DOMContentLoaded', () => {
         localStorage.setItem('themePreference', e.target.id)
     ));
 
+
     // * ===============================
     // * BOOTSTRAP TOOLTIP INITIALLIZE
     // * ===============================
+
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+
+    // * ================================
+    // * PLAN TOGGLE BUTTON
+    // * ================================
+
+    const toggle = document.getElementById('toggle');
+    const monthlyBtn = document.getElementById('monthly');
+    const yearlyBtn = document.getElementById('yearly');
+    const pro = document.querySelector(".pro h2");
+    const business = document.querySelector(".business h2");
+    console.log(pro, business);
+
+    monthlyBtn.addEventListener('click', () => {
+        toggle.classList.remove('yearly');
+        monthlyBtn.classList.add('active');
+        yearlyBtn.classList.remove('active');
+        pro.innerHTML = `<p class="text-white">$</p> 12`;
+        business.innerHTML = `<p>$</p> 28`;
+    });
+
+    yearlyBtn.addEventListener('click', () => {
+        toggle.classList.add('yearly');
+        yearlyBtn.classList.add('active');
+        monthlyBtn.classList.remove('active');
+        pro.innerHTML = `<p class="text-white">$</p> 8`;
+        business.innerHTML = `<p>$</p> 16`;
+    });
 
 });
 
